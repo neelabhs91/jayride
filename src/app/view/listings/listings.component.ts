a64605755e768cf8d7946615744318750e5e93fd
neelabhs91 <neelabhs91@gmail.com>
2023-05-18 22:11:04 +0800

    Update listings.component.ts

--- src/app/view/listings/listings.component.ts
+++ src/app/view/listings/listings.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./listings.component.css'],
 })
 export class ListingsComponent {
- passengers = ['passenger1', 'passenger2', 'passenger3'];
+ passengers: string[] = ['passenger1', 'passenger2', 'passenger3'];
 }
